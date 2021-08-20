e595b3b3298b879090e00a9848b40341bc1ee843
Dulan Manujaya <dulan69@gmail.com>
2021-08-20 11:36:22 +0530

    Add project files.

---
+++ videojs-wavesurferjs/Controllers/AudioController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace videojs_wavesurferjs.Controllers
+{
+    [ApiController]
+    [Route("/api/Audio")]
+    public class AudioController : ControllerBase
+    {
+        private readonly ILogger<AudioController> _logger;
+
+        public AudioController(ILogger<AudioController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("trim")]
+        public void Trim(string inPath, string outPath, string start, string end)
+        {
+            using (WaveFileReader reader = new WaveFileReader(inPath))
+            {
+                using (WaveFileWriter writer = new WaveFileWriter(outPath, reader.WaveFormat))
+                {
+                    TimeSpan cutFromStart = TimeSpan.Parse(start);
+                    TimeSpan cutFromEnd = TimeSpan.Parse(end);
+                    int bytesPerMillisecond = reader.WaveFormat.AverageBytesPerSecond / 1000;
+
+                    int startPos = (int)cutFromStart.TotalMilliseconds * bytesPerMillisecond;
+                    startPos = startPos - startPos % reader.WaveFormat.BlockAlign;
+
+                    int endBytes = (int)cutFromEnd.TotalMilliseconds * bytesPerMillisecond;
+                    endBytes = endBytes - endBytes % reader.WaveFormat.BlockAlign;
+                    int endPos = (int)reader.Length - endBytes;
+
+                    TrimWavFile(reader, writer, startPos, endPos);
+                }
+            }
+        }
+
+        [HttpGet]
+        [Route("convert")]
+        public void Convert(string inPath, string outPath)
+        {
+            using (WaveFileReader reader = new WaveFileReader(inPath))
+            {
+                using (WaveStream pcmStream = WaveFormatConversionStream.CreatePcmStream(reader))
+                {
+                    WaveFileWriter.CreateWaveFile(outPath, pcmStream);
+                }
+            }
+        }
+
+        private static void TrimWavFile(WaveFileReader reader, WaveFileWriter writer, int startPos, int endPos)
+        {
+            reader.Position = startPos;
+            byte[] buffer = new byte[1024];
+            while (reader.Position < endPos)
+            {
+                int bytesRequired = (int)(endPos - reader.Position);
+                if (bytesRequired > 0)
+                {
+                    int bytesToRead = Math.Min(bytesRequired, buffer.Length);
+                    int bytesRead = reader.Read(buffer, 0, bytesToRead);
+                    if (bytesRead > 0)
+                    {
+                        writer.WriteData(buffer, 0, bytesRead);
+                    }
+                }
+            }
+            
+        }
+
+        
+
+    }
+}
